8d466cd8e3607a636afd420a211dc1e70df6c133
na57 <na57@qq.com>
2017-02-24 11:28:29 +0800

    调整deviceManager结构

---
+++ src/models/manager-base.js
@@ -0,0 +1,37 @@
+import { Client } from 'ssh2';
+import { info } from '../config';
+
+export default class ManagerBase {
+  constructor(sshOptions) {
+    this.sshOptions = sshOptions;
+  }
+
+  /*
+登录ssh，并执行命令
+   */
+  run(commandHandler) {
+    const conn = new Client();
+    return new Promise((resolve, reject) => {
+      conn.on('ready', () => {
+        info('Client :: ready');
+        let str = '';
+        conn.shell((err, stream) => {
+          if (err) reject(err);
+          stream.on('close', () => {
+            conn.end();
+          }).on('data', (data) => {
+            str += data;
+          }).stderr.on('data', (data) => {
+            reject(data);
+          }).on('end', () => {
+            const result = commandHandler.handle(str);
+            resolve(result);
+          });
+          stream.write('screen-length disable\n');
+          stream.write(`${commandHandler.command}\n`);
+          stream.end('exit\n');
+        });
+      }).connect(this.sshOptions);
+    });
+  }
+}
