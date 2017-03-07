e6fb94ba87d89a483bdf52373e77fc90c9b1a96e
na57 <na57@qq.com>
2017-03-07 13:46:36 +0800

    test failed

--- src/index.js
+++ src/index.js
@@ -24,9 +24,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-const morgan = require('morgan');
-app.use(morgan('dev'));
-
 /*
 注册API
 */
