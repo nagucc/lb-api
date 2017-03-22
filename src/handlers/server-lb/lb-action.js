0ff77067fb6d1cd87b275929e89c8809c77c3f29
na57 <na57@qq.com>
2017-03-22 11:11:22 +0800

    处理异常

--- src/handlers/server-lb/lb-action.js
+++ src/handlers/server-lb/lb-action.js
@@ -1,5 +1,5 @@
 import CommandHandler from '../command-handler';
-import { info } from '../../config';
+import { info, error } from '../../config';
 
 /*
 负责均衡策略
@@ -32,14 +32,19 @@ LB action: to-docker-development-http-group
 
 const handleAction = (data) => {
   info('action text:', JSON.stringify(data));
-  const result = /LB action:(.*)\r*\n\s*Description:(.*)\r*\n\s*Type:(.*)\r*\n\s*State:(.*)\r*\n.*\r*\n\s*Server farm:(.*)/gm.exec(data);
-  return {
-    name: result[1].trim(),
-    description: result[2].trim(),
-    type: result[3].trim(),
-    state: result[4].trim(),
-    serverFarm: result[5].trim(),
-  };
+  try {
+    const result = /LB action:(.*)\r*\n\s*Description:(.*)\r*\n\s*Type:(.*)\r*\n\s*State:(.*)\r*\n.*\r*\n\s*Server farm:(.*)/gm.exec(data);
+    return {
+      name: result[1].trim(),
+      description: result[2].trim(),
+      type: result[3].trim(),
+      state: result[4].trim(),
+      serverFarm: result[5].trim(),
+    };
+  } catch (e) {
+    error('lb-action handle异常. action text:', JSON.stringify(data));
+    return {};
+  }
 };
 const handleResult = (data) => {
   info('display loadbalance action result:', JSON.stringify(data));
